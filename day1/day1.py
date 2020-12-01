269bd5a681aa9e2954fa12e22f5e0371f06de229
Julian Santos <julian.f.santos88@gmail.com>
2020-12-01 16:10:25 -0500

    part 2

--- day1/day1.py
+++ day1/day1.py
@@ -9,4 +9,12 @@ if __name__ == '__main__':
                 expense_sum = expense_array[i] + expense_array[j]
                 if expense_sum == 2020:
                     expense_mult = expense_array[i] * expense_array[j]
-                    print(expense_array[i], expense_array[j], expense_mult)
+                    print("Part 1: ", expense_array[i], expense_array[j], expense_mult)
+    for i in range(len(expense_array)):
+        for j in range(len(expense_array)):
+            for k in range(len(expense_array)):
+                if i != j and i != k and j != k:
+                    expense_sum = expense_array[i] + expense_array[j] + expense_array[k]
+                    if expense_sum == 2020:
+                        expense_mult = expense_array[i] * expense_array[j] * expense_array[k]
+                        print("Part 2: ", expense_array[i], expense_array[j], expense_array[k], expense_mult)
